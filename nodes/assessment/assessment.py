3450c60107c9fa7945e7456595bd2643dbd74e60
krasoa <aleks.krasowski@outlook.de>
2023-07-24 17:53:43 +0200

    feature: specificity metric

--- nodes/assessment/assessment.py
+++ nodes/assessment/assessment.py
@@ -52,15 +52,33 @@ while True:
     for target_class in sorted(np.unique(y_true)):
       y_true_c = [1 if y == target_class else 0 for y in y_true]
       y_pred_c = [1 if y == target_class else 0 for y in y_pred] 
+
+      tn, fp, fn, tp = m.confusion_matrix(y_true_c, y_pred_c).ravel()
   
       scores[str(target_class)] = {
-        "accuracy": m.accuracy_score(y_true_c, y_pred_c),
-        "f1": m.f1_score(y_true_c, y_pred_c),
-        "precision": m.precision_score(y_true_c, y_pred_c),
-        "recall": m.recall_score(y_true_c, y_pred_c),
-        # "support": int(sum(y_true_c))
+        "accuracy": (tp + tn) / (tp + tn + fp + fn),
+        "f1": 2 * tp / (2 * tp + fp + fn),
+        "precision": tp / (tp + fp),
+        "sensitivity": tp / (tp + fn),
+        "specificity": tn / (tn + fp)
       }  
+      
       for k, v in scores[str(target_class)].items():
         scores[str(target_class)][k] = np.round(v, 3)
+
+  specificities = [] 
+  if len(scores.keys()) > 1:
+    for v in scores.values():
+      specificities.append(v['specificity'])
+  else:
+    cm = m.confusion_matrix(y_true, y_pred)
+    for e, c in enumerate(np.unique(y_true)):
+      tn = np.sum(cm) - np.sum(cm[e, :]) - np.sum(cm[:, e]) + cm[e][e]
+      fp = np.sum(cm[:,e]) - cm[e,e]
+
+      specificities.append( tn / (tn + fp))
+      
+
+  scores['Total']['specificity'] = np.round(np.mean(specificities),3)
 
   print(json.dumps(scores))
